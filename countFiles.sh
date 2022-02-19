d6c7dc91ec97fded8ec78077a678a90d78c6af64
KaShTaN27 <95852548+KaShTaN27@users.noreply.github.com>
2022-02-19 09:56:37 +0300

    Add files via upload

---
+++ countFiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+params=2
+deniedSymbol='/'
+if [ "$#" -ne "$params" ]
+then
+  {
+    echo "Script takes two parameters:"
+    echo "  1 - catalog;"
+    echo "  2 - name of output file;"
+  } >&2
+exit 0
+fi
+
+if ! [ -d $1 ]
+then
+  echo "Error: There is no such catalog" >&2
+  exit 0
+fi
+
+case $2 in
+*"$deniedSymbol"*)
+echo "Error: Invalid output file name" >&2
+exit 0
+;;
+esac
+
+echo -n > $2
+for i in `find $1 -type d`
+do
+    echo "Viewed files: $(find $i -maxdepth 1 -type f | wc -l)" >> $2
+    echo "Total size: $(du -bsh $i)" >> $2
+done
+cat $2
