d664dadeed3694420e9f0fa670b26fc8926889c6
KaShTaN27 <95852548+KaShTaN27@users.noreply.github.com>
2022-03-18 01:26:36 +0300

    Create task3.c

---
+++ lab_2/task3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        perror("Unexpected number of parameters.");
+        return -1;
+    }
+
+    FILE* file;
+    file = (fopen(argv[1], "w"));
+    if (file == NULL){
+        perror("Error during opening file");
+        return -1;
+    }
+
+    char symbol;
+    symbol = (char) getc(stdin);
+    while (symbol != 6) {
+        fputc(symbol, file);
+        symbol = (char) getc(stdin);
+    }
+
+    if (fclose(file)) {
+        perror("Error during closing file");
+        return -1;
+    }
+    return 0;
+}
