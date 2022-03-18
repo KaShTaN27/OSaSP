f63ffb856649c883f6479b551c6c3add6eec9c91
KaShTaN27 <95852548+KaShTaN27@users.noreply.github.com>
2022-03-19 00:40:14 +0300

    Add files via upload

--- lab_2/task3.c
+++ lab_2/task3.c
@@ -1,32 +1,32 @@
-//#include <stdio.h>
-//#define CTRL_F 6
-//
-//int main(int argc, char *argv[]) {
-//
-//    if (argc != 2) {
-//        perror("Unexpected number of parameters.");
-//        return -1;
-//    }
-//
-//
-//
-//    FILE* file;
-//    file = (fopen(argv[1], "w"));
-//    if (file == NULL){
-//        perror("Error during opening file");
-//        return -1;
-//    }
-//
-//    char symbol;
-//    symbol = (char) getc(stdin);
-//    while (symbol != CTRL_F) {
-//        fputc(symbol, file);
-//        symbol = (char) getc(stdin);
-//    }
-//
-//    if (fclose(file)) {
-//        perror("Error during closing file");
-//        return -1;
-//    }
-//    return 0;
-//}
+#include <stdio.h>
+#define CTRL_F 6
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        perror("Unexpected number of parameters.");
+        return -1;
+    }
+
+
+
+    FILE* file;
+    file = (fopen(argv[1], "w"));
+    if (file == NULL){
+        perror("Error during opening file");
+        return -1;
+    }
+
+    char symbol;
+    symbol = (char) getc(stdin);
+    while (symbol != CTRL_F) {
+        fputc(symbol, file);
+        symbol = (char) getc(stdin);
+    }
+
+    if (fclose(file)) {
+        perror("Error during closing file");
+        return -1;
+    }
+    return 0;
+}
